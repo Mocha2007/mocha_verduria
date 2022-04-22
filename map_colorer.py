c27607766adad925e290fb8a034e4836908089c0
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-04-22 11:34:19 -0400

    standardized map colors

--- map_colorer.py
+++ map_colorer.py
@@ -1,10 +1,11 @@
 # 1;255;255;0;West Erelae;x
 # used to determine map colors so I don't accidentally dupe
+# 65,025 unique land and sea provinces each
 
 def definition_csv(n: int, is_land: bool = True) -> str:
 	n_ = n * 64 # oscillate between red, vermillion, orange, amber, and back
 	green = n_ % 255 # deliberately excluding 255
-	red_or_blue = n_ // 255
+	red_or_blue = n_ // 255 % 256
 	return f"{n};255;{green};{red_or_blue};PROV_NAME;x" if is_land \
 		else f"{n};{red_or_blue};{green};255;Sea;x"
 
